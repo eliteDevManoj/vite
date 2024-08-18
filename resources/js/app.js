6c87fb0d1752a62f434a75404b7283e9772bff1f
eliteDevManoj <manojmanupanwar666@gmail.com>
2024-08-18 20:48:00 +0530

    updates

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,6 @@
 import './bootstrap';
 import '../css/app.css'; 
 
-
 import '../../public/assets/libs/choices.js/public/assets/scripts/choices.min.js';
 import '../../public/assets/libs/@popperjs/core/umd/popper.min.js';
 import '../../public/assets/libs/tippy.js/tippy-bundle.umd.min.js';
@@ -10,5 +9,4 @@ import '../../public/assets/libs/prismjs/prism.js';
 import '../../public/assets/libs/lucide/umd/lucide.js';
 import '../../public/assets/js/tailwick.bundle.js';
 import '../../public/assets/js/pages/auth-login.init.js';
-
 import '../../public/assets/js/layout.js'
